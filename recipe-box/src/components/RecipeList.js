e145402ca504d0410076ced684e9be53d4369b8d
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-10 14:05:51 +0200

    up

--- recipe-box/src/components/RecipeList.js
+++ recipe-box/src/components/RecipeList.js
@@ -1,13 +1,34 @@
 import React from "react";
 import RenderRecipe from "./RenderRecipe";
 
-export default class RecipeList extends React.Component{
-    render(){
-        return(
-            <div>
-                <RenderRecipe/>
-            <button type="button">Add Recipe</button>
-            </div>
-        )
-    }
+export default class RecipeList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      ingredients: ""
+    };
+    this.addElement = this.addElement.bind(this);
+  }
+  addElement(e) {
+    e.preventDefault();
+    this.setState({
+      title: e.target.titleInput.value,
+      ingredients: e.target.ingredientsInput.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <RenderRecipe />
+        <form onSubmit={this.addElement}>
+          <h3>Title</h3>
+          <input type="text" name="titleInput" />
+          <h3>Ingredients</h3>
+          <input type="text" name="ingredientsInput" />
+          <input type="submit" value="Add Recipe" />
+        </form>
+      </div>
+    );
+  }
 }
--- recipe-box/src/components/RenderRecipe.js
+++ recipe-box/src/components/RenderRecipe.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export default class RenderRecipe extends React.Component {
   render() {
-
     let myJson = JSON.parse(localStorage.recipeBook);
     return myJson.map(item => {
-      return <p key={item._id}>{item.title}</p>;
+      return (
+        <div key={item._id}>
+          <h3>{item.title}</h3>
+          <p>{item.ingredients}</p>
+        </div>
+      );
     });
   }
 }
