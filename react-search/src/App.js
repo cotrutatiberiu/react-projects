7e5c8e907e1b74f965241c2458e7010b07883288
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-10 08:34:44 +0200

    up

--- react-search/src/App.js
+++ react-search/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import {fleet} from "./data/data";
 import Title from "./components/TitleBar";
 import SearchBar from "./components/SearchBar";
-import ListArtist from "./components/ListArtist";
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Title/>
-        <SearchBar/>
-        <ListArtist data={fleet}/>
+        <SearchBar data={fleet}/>
       </div>
     );
   }
--- react-search/src/components/SearchBar.js
+++ react-search/src/components/SearchBar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import ListArtist from "./ListArtist";
 
 export default class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value:""
+        };
+        this.addToSearch=this.addToSearch.bind(this);
+    }
+    addToSearch(e){
+        this.setState({value:e.target.value});
+    }
     render(){
         return(
             <div>
             <h1>{this.props.searchTitle}</h1>
-            <input type="text" placeholder={this.props.searchPlaceholder}/>
+            <input type="text" placeholder={this.props.searchPlaceholder} value={this.state.value} onChange={this.addToSearch}/>
+            <ListArtist data={this.props.data}/>
             </div>
         )
     }
