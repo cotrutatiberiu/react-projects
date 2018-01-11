a535624c260a3b0b32f8d6819d092b88c875a7d3
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-11 10:43:40 +0200

    test

--- recipe-box/src/components/RenderRecipe.js
+++ recipe-box/src/components/RenderRecipe.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default class RenderRecipe extends React.Component {
+  deleteStuff(){
+    
+  }
   render() {
     let myJson = JSON.parse(localStorage.recipeBook);
     return myJson.map(item => {
@@ -8,13 +11,14 @@ export default class RenderRecipe extends React.Component {
         <div key={item._id}>
           <h3>{item.title}</h3>
           <p>{item.ingredients.join(" , ")}</p>
-          <button type="button">Delete</button>
+          <button type="submit" onSubmit={this.deleteStuff}>Delete</button>
           <button type="button>">Edit</button>
+
           <form onSubmit={this.addElement}>
-          <input type="text" name="titleInput" />
-          <input type="text" name="ingredientsInput" />
-          <input type="submit" value="Submit Edit" />
-        </form>
+            <input type="text" name="titleInput" />
+            <input type="text" name="ingredientsInput" />
+            <input type="submit" value="Submit Edit" />
+          </form>
         </div>
       );
     });
