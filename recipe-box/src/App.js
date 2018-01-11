e90de268af3e8253b59fa2d72d01c9140e6e0a4b
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-11 09:26:58 +0200

    a

--- recipe-box/src/App.js
+++ recipe-box/src/App.js
@@ -8,15 +8,21 @@ import { fleet } from "./data/data-file";
 class App extends Component {
   componentWillMount() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     if (localStorage.recipeBook === undefined) {
       localStorage.setItem("recipeBook", JSON.stringify(fleet));
     }
 =======
+=======
+>>>>>>> parent of 728bee5... up
     let fleetFix = fleet;
     for (let i = 0; i < fleet.length; i++) {
       fleetFix[i]._id = `${i}`;
     }
     localStorage.setItem("recipeBook", JSON.stringify(fleetFix));
+<<<<<<< HEAD
+>>>>>>> parent of 728bee5... up
+=======
 >>>>>>> parent of 728bee5... up
   }
   render() {
--- recipe-box/src/components/RecipeList.js
+++ recipe-box/src/components/RecipeList.js
@@ -27,8 +27,12 @@ export default class RecipeList extends React.Component {
       ingredients: e.target.ingredientsInput.value
     });
 <<<<<<< HEAD
+<<<<<<< HEAD
     this.state.myJson.push({ title: e.target.titleInput.value, ingredients: e.target.ingredientsInput.value.split(" ") });
     localStorage.setItem("recipeBook", this.state.myJson);
+=======
+    console.log(this.state.title);
+>>>>>>> parent of 728bee5... up
 =======
     console.log(this.state.title);
 >>>>>>> parent of 728bee5... up
