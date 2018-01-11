e0eee7cd8a0f6bf77dd1f2e0ae222fd4630e5015
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-11 10:13:47 +0200

    up

--- recipe-box/src/components/RenderRecipe.js
+++ recipe-box/src/components/RenderRecipe.js
@@ -8,6 +8,13 @@ export default class RenderRecipe extends React.Component {
         <div key={item._id}>
           <h3>{item.title}</h3>
           <p>{item.ingredients.join(" , ")}</p>
+          <button type="button">Delete</button>
+          <button type="button>">Edit</button>
+          <form onSubmit={this.addElement}>
+          <input type="text" name="titleInput" />
+          <input type="text" name="ingredientsInput" />
+          <input type="submit" value="Submit Edit" />
+        </form>
         </div>
       );
     });
