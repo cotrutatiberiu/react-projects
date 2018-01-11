1d327b6844767c0508ac8f550cc5e6d1d0f6452f
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-11 15:18:36 +0200

    Revert "updo"
    This reverts commit b1dcf0367e7c1511777ce24c8fbf8f60ae468fe1.

--- recipe-box/src/components/RecipeList.js
+++ recipe-box/src/components/RecipeList.js
@@ -24,19 +24,12 @@ export default class RecipeList extends React.Component {
 
     localStorage.setItem("recipeBook", JSON.stringify(this.state.myJson));
   }
-  componentWillMount(){
-    for (let i = 0; i < this.state.myJson.length; i++) {
-
-      // this.setState({myJson:this.state.myJson[i]._id = `${i}`});
-      let myJson=Object.assign({},this.state.myJson);
-      myJson._id=`${i}`;
-      this.setState({myJson});
-    }
-    console.log({this.state.myJson});
-    localStorage.setItem("recipeBook", JSON.stringify(this.state.myJson));
-  }
   render() {
-
+    let fixMyJson = this.state.myJson;
+    for (let i = 0; i < this.state.myJson.length; i++) {
+      fixMyJson[i]._id = `${i}`;
+    }
+    localStorage.setItem("recipeBook", JSON.stringify(fixMyJson));
     return (
       <div>
         <RenderRecipe data={this.state.myJson} />
