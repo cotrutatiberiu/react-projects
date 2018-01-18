a81b5821bb9c8975715f50f63741b906089cb05b
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-18 12:02:22 +0200

    up

--- leaderboard/src/components/DataKeeper.js
+++ leaderboard/src/components/DataKeeper.js
@@ -1,24 +1,21 @@
 import React from "react";
 
-export default class DataKeeper extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            alltime:[]
-        }
-    }
-    componentWillMount(){
-        fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime").then(results=>{
-            return results.json();
-        }).then(data=>{
-            let username =data.results.map((user)=>{
-                return <p>{user.alltime}</p>
-            })
-            this.setState({alltime:username});
-        })
-    }
-    render(){
-        console.log(this.state.alltime);
-        return <h1>{this.state.alltime}</h1>
-    }
+export default class DataKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alltime: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ alltime: data });
+      });
+  }
+  render() {
+    console.log(this.state.alltime);
+    return <h1>a</h1>;
+  }
 }
