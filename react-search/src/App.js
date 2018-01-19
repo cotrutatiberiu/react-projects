eac0639462069dde82b247bc35b6700b6430e620
Cotruta Tiberiu <cotruta.tiberiu@gmail.com>
2018-01-19 20:42:55 +0200

    up

--- react-search/src/App.js
+++ react-search/src/App.js
@@ -7,6 +7,7 @@ import SearchBar from "./components/SearchBar";
 
 export default class App extends React.Component {
   render() {
+    //a
     var AppStyle={
       backgroundColor:"#023436",
       height:"100vh",
