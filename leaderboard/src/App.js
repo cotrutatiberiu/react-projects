52791070f744ee3536389aa42cf73925d9c1237b
Cotruta Tiberiu <cotruta.tiberiu@gmail.com>
2018-01-21 21:17:54 +0200

    up

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
-import { fleet } from "./components/fleet";
 import Title from "./components/Title";
-import ButtonTable from "./components/ButtonTable";
+import Button from "./components/Button";
+
+function RenderItems(props) {
+  let i = 0;
+  return props.alltime.map(item => {
+    i++;
+    return <p key={i}>{item.username}</p>;
+  });
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -10,26 +17,41 @@ class App extends React.Component {
     this.state = {
       alltime: []
     };
+    this.updateShowRecent = this.updateShowRecent.bind(this);
+    this.updateShowAllTime=this.updateShowAllTime.bind(this);
+  }
+  updateShowRecent(e) {
+    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ alltime: data });
+      });
+  }
+  updateShowAllTime(e) {
+    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ alltime: data });
+      });
   }
   componentWillMount() {
-    // fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({ alltime: data });
-    //   });
-    this.setState({ alltime: fleet });
+    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ alltime: data });
+      });
   }
   render() {
-    // console.log(this.state.alltime);
-    // let i = 0;
-    // return this.state.alltime.map(item => {
-    //   i++;
-    //   return <p key={i}>{item.username}</p>;
-    // });
     return (
       <div>
         <Title title="Leaderboard" />
-        <ButtonTable/>
+        <table>
+          <th onClick={this.updateShowRecent}><Button buttonName="Show Recent" /></th>
+          <th onClick={this.updateShowAllTime}><Button buttonName="Show Alltime" /></th>
+          <th><Button buttonName="Sort by Recent" /></th>
+          <th><Button buttonName="Sort by Alltime" /></th>
+        </table>
+        <RenderItems alltime={this.state.alltime} />
       </div>
     );
   }
