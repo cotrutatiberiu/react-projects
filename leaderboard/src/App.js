283ab936aceeff75bb536e5dc60fc6f68bc26259
cotrutatiberiu <32967624+cotrutatiberiu@users.noreply.github.com>
2018-01-22 22:12:00 +0200

    up

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -2,71 +2,58 @@ import React from "react";
 import "./App.css";
 import Title from "./components/Title";
 import Button from "./components/Button";
-
-function RenderItems(props) {
-  return props.alltimeData.map((item, i) => {
-    return (
-      <tr key={i}>
-        <td>{item.username}</td>
-        <td>{item.recent}</td>
-        <td>{item.alltime}</td>
-        <td>{item.lastUpdate}</td>
-      </tr>
-    );
-  });
-}
+import RenderItems from "./components/RenderItems";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      alltimeData: []
+      alltimeData: [],
+      recentData: [],
+      toRender:[],
+      sorted:false
     };
-    this.updateShowRecent = this.updateShowRecent.bind(this);
-    this.updateShowAllTime = this.updateShowAllTime.bind(this);
-  }
-  updateShowRecent(e) {
-    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ alltimeData: data });
-      });
-  }
-  updateShowAllTime(e) {
-    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ alltimeData: data });
-      });
+    this.toSort=this.toSort.bind(this);
   }
   componentWillMount() {
     fetch("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
       .then(response => response.json())
       .then(data => {
-        this.setState({ alltimeData: data });
+        this.setState({ alltimeData: data,toRender:data });
+      });
+    fetch("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ recentData: data });
       });
   }
+  toSort(){
+    if(this.state.toRender===this.state.alltimeData){
+      
+    }else if(this.state.toRender===this.state.recentData){
+      
+    }
+  }
   render() {
-    console.log(this.state.alltimeData);
     return (
       <div>
         <Title title="Leaderboard" />
 
-        <div onClick={this.updateShowRecent}>
+        <div onClick={ ()=>this.setState({toRender:this.state.recentData,sorted:false})} id="a">
           <Button buttonName="Show Recent" />
         </div>
-        <div onClick={this.updateShowAllTime}>
+        <div onClick={()=>this.setState({toRender:this.state.alltimeData,sorted:false})} name="all" id="b">
           <Button buttonName="Show Alltime" />
         </div>
-        <div>
-          <Button buttonName="Sort by Recent" />
+        <div onClick={this.toSort}>
+          <Button buttonName="Sort by Recent"/>
         </div>
-        <div>
+        <div onClick={this.toSort}>
           <Button buttonName="Sort by Alltime" />
         </div>
         <table>
           <tbody>
-            <RenderItems alltimeData={this.state.alltimeData} />
+            <RenderItems alltimeData={this.state.toRender} />
           </tbody>
         </table>
       </div>
---
+++ leaderboard/src/components/RenderItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function RenderItems(props) {
+    return props.alltimeData.map((item, i) => {
+      return (
+        <tr key={i}>
+          <td>{item.username}</td>
+          <td>{item.recent}</td>
+          <td>{item.alltime}</td>
+          <td>{item.lastUpdate}</td>
+        </tr>
+      );
+    });
+  }
